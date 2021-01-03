b81006817bbb3614dde36cb50e3888bd53c6814c
somekindofgitusername <46464309+somekindofgitusername@users.noreply.github.com>
2021-01-03 14:33:21 +0100

    Update lls.py
    Cleanup

--- lls.py
+++ lls.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
-import sys, os, re, glob, fnmatch
 
-#======argparse===================================================================
+import sys
+import os
+import re
+import glob
+import fnmatch
 import argparse
+import itertools 
+import subprocess
 
-parser = argparse.ArgumentParser(description='Show filepatterns of numbered files')
-parser.add_argument('-s', '--size', help='display size of file patterns', action='store_true')
-parser.add_argument('-m', '--minimal', help='only show file patterns', action='store_true')
-parser.add_argument('-v', '--verbose', type=int, nargs="?", const=1, help='display elements of file patterns with | (present) and . (not present) ')#, action='store_true')
-
-args = parser.parse_args()
-#=======/argparse==================================================================
-class color:
+class Color:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
     DARKCYAN = '\033[36m'
@@ -23,48 +21,51 @@ class color:
     UNDERLINE = '\033[4m'
     END = '\033[0m'
 
-def condensedString(l, n):
+def parse_args():
+    """Parse arguments.
+    """
+    parser = argparse.ArgumentParser(description='Show filepatterns of numbered files')
+    parser.add_argument('-s', '--size', help='display size of file patterns', action='store_true')
+    parser.add_argument('-m', '--minimal', help='only show file patterns', action='store_true')
+    parser.add_argument('-v', '--verbose', type=int, nargs="?", const=1,
+                        help='display elements of file patterns with | (present) and . (not present) ')
 
+    args = parser.parse_args()
+    return args
 
+def condensed_string(l, n):
+    """Take a string like |||.|||..... and rewrite in
+    in such a way that characters appearing n times contigously
+    are reduced to one occurance 
+    """
     resultList = []
-    import itertools
     for grouping_value, group_items in itertools.groupby(l):
         grp = ''.join(list(group_items))
         grpLen = len(grp)
         multiple = (grpLen / n)
         remainder = grpLen%n
         itemType = grouping_value
-        condensedItemType = color.UNDERLINE + color.BOLD + color.RED + str(itemType) + color.END
+        condensedItemType = Color.UNDERLINE + Color.BOLD + Color.RED + str(itemType) + Color.END
         condensedValue = condensedItemType*multiple + itemType*remainder
-        #print itemType, grpLen, multiple, remainder, " --->", condensedValue
         resultList.append(condensedValue)
-        
+
     return " verbosity on ("+str(n)+"s):"+''.join(resultList)
 
-cwd = os.getcwd()
-#==================================================================================
+def atoi(text):
+    return int(text) if text.isdigit() else text
 
-#Find all files that contain a number and store their patterns
+def natural_keys(text):
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
 def contains_digit(s):
+    """Find all files that contain a number and store their patterns.
+    """
     isdigit = str.isdigit
     return any(map(isdigit,s))
 
-items = filter(lambda f: not f.startswith('.'), os.listdir(cwd) ) 
-items = filter(lambda f: not os.path.isdir(f), items)
-items = filter(contains_digit, items)
-
-filePatterns = {}
-for item in items:
-    m = re.search(r'\d+(?=\D*$)', item) #search for a number starting at the string's back
-    
-    frameNum = int(m.group(0))
-    fileItem = item.rsplit(str(frameNum), 1) #split only once from back
-    fileList = [fileItem[0], frameNum, fileItem[1]]
-    filePatterns[fileItem[0]]=[fileItem[0], fileItem[-1]]
-
-
-#Loop over file patterns and compute ranges
-def framesAscii(minFrameNum, maxFrameNum, frames):
+def frames_ascii(minFrameNum, maxFrameNum, frames):
+    """Loop over file patterns and compute ranges.
+    """
     framesRepresentation = []
     for i in  range(minFrameNum, maxFrameNum):
         if i in frames:
@@ -73,59 +74,87 @@ def framesAscii(minFrameNum, maxFrameNum, frames):
             framesRepresentation.append(".")
     return framesRepresentation
 
-result = []
-for k,v in filePatterns.items():
-    filePattern = "*".join(v)
-    filePatternMatches = fnmatch.filter(items, filePattern)
-    
-    minFrameNum = None
-    maxFrameNum = None
-    currentFrameNum = None
-    frames = []
-    for fileName in filePatternMatches:
-        m = re.search(r'\d+(?=\D*$)', fileName)
-        currentFrameNum = int(m.group(0))
-        frames.append(currentFrameNum) #list of all frames
-        if minFrameNum==None:
-            minFrameNum = currentFrameNum
-            maxFrameNum = currentFrameNum
-        else:    
-            minFrameNum = min(minFrameNum, currentFrameNum)
-            maxFrameNum = max(maxFrameNum, currentFrameNum)
+def get_work_items(cwd):
+    """Return work items aka the list of files to process.
+    """
+    items = filter(lambda f: not f.startswith('.'), os.listdir(cwd) ) 
+    items = filter(lambda f: not os.path.isdir(f), items)
+    items = filter(contains_digit, items)
+    filePatterns = {}
+    for item in items:
+        m = re.search(r'\d+(?=\D*$)', item) #search for a number starting at the string's back
+        
+        frameNum = int(m.group(0))
+        fileItem = item.rsplit(str(frameNum), 1) #split only once from back
+        filePatterns[fileItem[0]]=[fileItem[0], fileItem[-1]]
+    return(filePatterns, items)
 
-    #------ arguments and options ----------------
-    if args.minimal:
-        message = filePattern
-    else:
-        message = filePattern + "\t----> has range: "+ str(minFrameNum) +"..." + str(maxFrameNum) +"\tcompleted( "+ str(len(filePatternMatches)) + " )"
 
-    #adds ASCII representation of frames
-    if args.verbose:
-        visualMessage = "\nframes: " + "".join( framesAscii(minFrameNum, maxFrameNum, frames) ) 
-        if args.verbose>1:
-            visualMessage = condensedString(visualMessage, args.verbose)
-        message += visualMessage
+#===== MAIN =======================================================================
 
-    #add total filesize per filepattern
-    if args.size:
-        import subprocess
-        cmd = "du -h -c "+filePattern+" | tail -1"
-        cmdOut = subprocess.check_output(cmd, shell=True).split()[0]
-        message += " total size: "+ str(cmdOut)
-    #------/arguments and options ----------------    
-    
-    result.append(message)
+def main():
+    """ Loop over relevant files (work items) in current directory and
+    extract their number terminated patterns. Give additional 
+    information according to command options.
+    """
+    cwd = os.getcwd()
 
-#==================================================================================
-#print sorted file patterns with ranges
-def atoi(text):
-    return int(text) if text.isdigit() else text
+    args = parse_args()
+    workItems = get_work_items(cwd)
+    filePatterns = workItems[0]
+    items = workItems[1]
 
-def natural_keys(text):
-    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+    result = []
+    for k,v in filePatterns.items():
+        filePattern = "*".join(v)
+        filePatternMatches = fnmatch.filter(items, filePattern)
+        
+        minFrameNum = None
+        maxFrameNum = None
+        currentFrameNum = None
+        
+        frames = []
+        for fileName in filePatternMatches:
+            m = re.search(r'\d+(?=\D*$)', fileName)
+            currentFrameNum = int(m.group(0))
+            frames.append(currentFrameNum) #list of all frames
+            if minFrameNum==None:
+                minFrameNum = currentFrameNum
+                maxFrameNum = currentFrameNum
+            else:    
+                minFrameNum = min(minFrameNum, currentFrameNum)
+                maxFrameNum = max(maxFrameNum, currentFrameNum)
 
-result.sort(key=natural_keys)   
+        #------ arguments and options ------------------------------
+        if args.minimal:
+            message = filePattern
+        else:
+            message = filePattern + "\t----> has range: "+ str(minFrameNum) +"..."
+            message += str(maxFrameNum) +"\tcompleted( "+ str(len(filePatternMatches)) + " )"
 
-for thing in result:
-    print thing
+        #adds ASCII representation of frames
+        if args.verbose:
+            visualMessage = "\nframes: " + "".join( frames_ascii(minFrameNum, maxFrameNum, frames) ) 
+            if args.verbose>1:
+                visualMessage = condensed_string(visualMessage, args.verbose)
 
+            message += visualMessage
+
+        #add total filesize per filepattern
+        if args.size:
+            cmd = "du -h -c "+filePattern+" | tail -1"
+            cmdOut = subprocess.check_output(cmd, shell=True).split()[0]
+            message += " total size: "+ str(cmdOut)
+        #------/arguments and options -----------------------------
+        
+        result.append(message)
+
+
+    result.sort(key=natural_keys)   
+    for thing in result:
+        print thing
+
+#===== /MAIN ========================================================================
+
+if __name__ == '__main__':
+    sys.exit(main())
