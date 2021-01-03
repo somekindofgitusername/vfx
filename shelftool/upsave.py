c9c5dea8932f3f2dc115d11069939c108d84f6ea
somekindofgitusername <46464309+somekindofgitusername@users.noreply.github.com>
2021-01-03 19:44:14 +0100

    Create upsave.py

---
+++ shelftool/upsave.py
@@ -0,0 +1,9 @@
+# for use as a houdini shelf tool
+# upsaves the file
+
+print '\n====================\n'
+print 'current filename ',hou.hipFile.basename()
+print ' ... upversion save ...'
+hou.hipFile.saveAndIncrementFileName()
+print 'current filename ',hou.hipFile.basename()
+print '\n---------------------\n'
