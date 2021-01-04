1c06db936d810b61f4356750ea3e09ed07b454dd
somekindofgitusername <marc.picco@gmail.com>
2021-01-04 14:54:13 +0100

    first rough

---
+++ shelftool/renderseq.py
@@ -0,0 +1,94 @@
+# hou tool to render non contigous sequences
+
+import os.path
+from os import path
+import time, os
+
+print ("\n\n===========================")
+hou.hipFile.save()
+
+nums_node = hou.node("/stage/nums")
+frames = map(int, nums_node.parm("nums").eval().split())
+print (frames)
+rs_node = hou.selectedNodes()[0]
+print (rs_node)
+image_path = rs_node.parm("picture").eval()
+# imagesize = str( rs_node.parm('resolutionx').eval() ) + '_x_' +str( rs_node.parm('RS_overrideRes2').eval() )
+n = rs_node
+xs = str(rs_node.parmTuple(n.path() + "/resolution")[0].eval())
+ys = str(rs_node.parmTuple(n.path() + "/resolution")[0].eval())
+imagesize = xs + "_x_" + ys
+
+
+for f in frames:
+    print ("\n ... frame ", str(f))
+    hou.setFrame(f)
+    image_path = rs_node.parm("picture").eval()
+
+    if path.exists(image_path):
+        print image_path, " exists"
+
+    else:
+        print image_path, " does not exist"
+
+        rs_node.parm("execute").pressButton()
+        path_is = path.exists(image_path)
+        path_is_not = not path_is
+        while path_is_not:
+            print " ... waiting for file ... ", image_path
+            time.sleep(20)
+            path_is = path.exists(image_path)
+            path_is_not = not path_is
+            if path_is:
+                break
+
+        print "\n out of while for path", image_path
+
+        hipfilename = hou.hipFile.path()
+        mycommand = "setfattr -n user.hipfile -v " + hipfilename + " " + image_path
+        mycommand2 = "setfattr -n user.frame -v " + str(hou.frame()) + " " + image_path
+        mycommand3 = (
+            "setfattr -n user.houversion -v "
+            + hou.applicationVersionString()
+            + " "
+            + image_path
+        )
+        mycommand4 = "setfattr -n user.imagesize -v " + imagesize + " " + image_path
+        print "mycommand ", mycommand
+        print "mycommand2 ", mycommand2
+        os.system(mycommand)
+        os.system(mycommand2)
+        os.system(mycommand3)
+        os.system(mycommand4)
+        try:
+            lut = hou.parm("/obj/cam1/RS_campro_lutFile").eval()
+            env = hou.parm("/stage/domelight1/xn__texturefile_0ta").eval()
+            dist = (
+                hou.node(
+                    hou.parm("/stage/camera1/xn__karmacameralensshadervop_4fbg").eval()
+                )
+                .parm("curv_map")
+                .eval()
+            )
+            foc = (
+                hou.node(
+                    hou.parm("/stage/camera1/xn__karmacameralensshadervop_4fbg").eval()
+                )
+                .parm("focus_map")
+                .eval()
+            )
+            mycommand5 = "setfattr -n user.lut -v " + lut + " " + image_path
+            mycommand6 = "setfattr -n user.env -v " + env + " " + image_path
+            mycommand7 = "setfattr -n user.distort -v " + dist + " " + image_path
+            mycommand8 = "setfattr -n user.focus -v " + foc + " " + image_path
+            os.system(mycommand5)
+            os.system(mycommand6)
+            os.system(mycommand7)
+            os.system(mycommand8)
+        except:
+            pass
+
+import os
+
+os.system("shutdown")
+# os.system('systemctl suspend')
