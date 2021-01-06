de8262acca4180ecb1adf61f0ca18468d84b1c15
somekindofgitusername <marc.picco@gmail.com>
2021-01-06 15:44:53 +0100

    First commit

---
+++ SOP/wedge_renumbering.py
@@ -0,0 +1,21 @@
+# Python sop tool
+#
+# wedge renumbering such that a non-contigous sequence will be made contigous
+
+node = hou.pwd()
+geo = node.geometry()
+
+clusterName = "cluster"
+
+# list of existing clusters
+clusters = list(set([int(p.attribValue(clusterName)) for p in geo.points()]))
+
+# remap clusters into contigous order starting at 0
+dic = {t[1]: t[0] for t in enumerate(clusters)}
+print "cluster remap {old_cluster:new_cluster} = ", dic
+
+# assign new cluster number
+[
+    p.setAttribValue("cluster", float(dic[p.attribValue("cluster")]))
+    for p in geo.points()
+]
