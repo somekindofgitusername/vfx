85933b6cd3b79147d1467a72721157dbea3fc105
somekindofgitusername <marc.picco@gmail.com>
2021-01-09 13:49:01 +0100

    cleanup

--- shelftool/omerge.py
+++ shelftool/omerge.py
@@ -18,16 +18,21 @@ def omerge():
     for n in hou.selectedNodes():
         print("node: ", str(n))
 
+        # Get node data
         node_path = n.path()
         n_pos = n.position()
+
+        # Create object_merge node
         m = n.parent().createNode("object_merge")
         print("node parent: ", n.parent())
 
+        # Calculate object_merge node's position
         offset_y = n.size()[1]
         v2 = hou.Vector2((0, offset_y))
         m_pos = n_pos - 3 * v2
         m.move(m_pos)
 
+        # Set Parms of object_merge node
         parm_dict = {}
         parm_dict["numobj"] = 2
         parm_dict["objpath1"] = node_path
@@ -37,6 +42,7 @@ def omerge():
 
         name = "_merge_" + n.name()
 
+        # Set Flags on object_merge_node
         m.setName(name, True)
         m.setColor(hou.Color((0, 0, 0)))
         m.setDisplayFlag(True)
